339ff2334dc130dda6262d20c6a8752a50d072d8
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2024-02-13 04:08:15 +0700

    Write buffer content into /tmp/<file>

--- src/backend.rs
+++ src/backend.rs
@@ -1,8 +1,11 @@
 use crate::Config;
-use tower_lsp::{jsonrpc, lsp_types as lsp, LanguageServer};
+use smol::{fs, io, lock::OnceCell};
+use std::{env, path::PathBuf};
+use tower_lsp::{jsonrpc, lsp_types as lsp, Client, LanguageServer};
 
-/// TODO
 pub struct Backend {
+    tempdir: OnceCell<PathBuf>,
+    client: Client,
     config: Config,
 }
 
@@ -12,6 +15,18 @@ impl LanguageServer for Backend {
         &self,
         params: lsp::InitializeParams,
     ) -> jsonrpc::Result<lsp::InitializeResult> {
+        use std::{collections::hash_map::DefaultHasher, hash::Hasher};
+
+        if let Some(pid) = params.process_id {
+            let tempdir = {
+                let mut hasher = DefaultHasher::new();
+                let cwd = env::current_dir().expect("need permission");
+                env::temp_dir().join(format!("lspcat-{}", hasher.finish()))
+            };
+            let _ = fs::create_dir(&tempdir).await;
+            self.tempdir.set_blocking(tempdir).expect("must set once"); // WARNING: using async version didn't works
+        }
+
         Ok(lsp::InitializeResult {
             capabilities: lsp::ServerCapabilities {
                 text_document_sync: Some(lsp::TextDocumentSyncCapability::Options(
@@ -28,6 +43,50 @@ impl LanguageServer for Backend {
             },
             ..Default::default()
         })
+    }
+
+    async fn did_open(&self, params: lsp::DidOpenTextDocumentParams) {
+        use io::AsyncWriteExt;
+        use std::path::Path;
+
+        let doc = params.text_document;
+        let Some(tempdir) = self.tempdir.get() else {
+            return;
+        };
+        let cwd = env::current_dir().expect("need permission");
+        let Ok(path) = Path::new(doc.uri.path())
+            .strip_prefix(cwd)
+            .map(|file| tempdir.join(file))
+        else {
+            return;
+        };
+        if let Some(dir) = path.parent() {
+            if let Err(err) = fs::create_dir_all(dir).await {
+                return self.client.log_message(lsp::MessageType::ERROR, err).await;
+            }
+        }
+
+        let Ok(mut file) = fs::OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .open(&path)
+            .await
+        else {
+            return;
+        };
+        let write_result = file.write_all(doc.text.as_bytes()).await;
+        let sync_result = file.sync_data().await;
+        if let Err(err) = write_result.and(sync_result) {
+            self.client.log_message(lsp::MessageType::ERROR, err).await;
+        }
+
+        self.client
+            .log_message(
+                lsp::MessageType::LOG,
+                format!("edit {} as {}", doc.uri.path(), path.display()),
+            )
+            .await;
     }
 
     async fn shutdown(&self) -> jsonrpc::Result<()> {
